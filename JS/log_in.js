a926dbf2aba511a0ad5136df54a0717678a71918
ipiteuber <140553021+ipiteuber@users.noreply.github.com>
2025-04-01 19:20:40 -0300

    Implementacion roles

--- JS/log_in.js
+++ JS/log_in.js
@@ -5,11 +5,16 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     const username = document.getElementById("loginUser").value;
     const password = document.getElementById("loginPassword").value;
 
-    // Autenticacion
+    // Autenticacion con roles (usuario normal y vendedor)
     if (username === "user123" && password === "password123") {
-        localStorage.setItem("user", username); // Guarda sesion en localStorage
-        window.location.href = "success.html"; // Redirige a la pagina success
+        localStorage.setItem("user", username);
+        localStorage.setItem("role", "user"); // Asigna el rol de usuario
+        window.location.href = "welcome_log.html"; // Redirige a pagina de usuario
+    } else if (username === "vendedor" && password === "passwordVendedor") {
+        localStorage.setItem("user", username);
+        localStorage.setItem("role", "vendedor"); // Asigna el rol de vendedor
+        window.location.href = "welcome_role.html"; // Redirige a pagina de vendedor
     } else { // Mensaje en caso de error
-        alert("Invalid username or password. Please try again."); 
+        alert("Invalid username or password. Please try again.");
     }
 });
