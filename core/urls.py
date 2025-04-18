58cf9f412c050979faeb8ef184a4d23af042b82a
ipiteuber <140553021+ipiteuber@users.noreply.github.com>
2025-04-18 10:09:34 -0400

    Correcion links href HTML

---
+++ core/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('cart/', views.cart, name='cart'),
+    path('services/', views.services, name='services'),
+    path('coaches/', views.coaches, name='coaches'),
+
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('code-password/', views.code_password, name='code_password'),
+    path('new-password/', views.new_password, name='new_password'),
+    path('success-checkout/', views.success_checkout, name='success_checkout'),
+    path('success-password/', views.success_password, name='success_password'),
+    path('success/', views.success, name='success'),
+    path('welcome-log/', views.welcome_log, name='welcome_log'),
+    path('welcome-role/', views.welcome_role, name='welcome_role'),
+
+    path('coaches/s1mple/', views.s1mple, name='s1mple'),
+    path('coaches/sinatraa/', views.sinatraa, name='sinatraa'),
+    path('coaches/tarik/', views.tarik, name='tarik'),
+    path('coaches/tenz/', views.tenz, name='tenz'),
+    path('coaches/xqc/', views.xqc, name='xqc'),
+    path('coaches/zellsis/', views.zellsis, name='zellsis'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Index
+def index(request):
+    return render(request, 'index/index.html')
+
+def cart(request):
+    return render(request, 'index/cart.html')
+
+def services(request):
+    return render(request, 'index/services.html')
+
+def coaches(request):
+    return render(request, 'index/coaches.html')
+
+# Account
+def login(request):
+    return render(request, 'account/log_in.html')
+
+def signup(request):
+    return render(request, 'account/sign_up.html')
+
+def profile(request):
+    return render(request, 'account/profile.html')
+
+def forgot_password(request):
+    return render(request, 'account/forgot_password.html')
+
+def code_password(request):
+    return render(request, 'account/code_password.html')
+
+def new_password(request):
+    return render(request, 'account/new_password.html')
+
+def success_checkout(request):
+    return render(request, 'account/success_checkout.html')
+
+def success_password(request):
+    return render(request, 'account/success_password.html')
+
+def success(request):
+    return render(request, 'account/success.html')
+
+def welcome_log(request):
+    return render(request, 'account/welcome_log.html')
+
+def welcome_role(request):
+    return render(request, 'account/welcome_role.html')
+
+# Coaches
+def s1mple(request):
+    return render(request, 'coaches/s1mple.html')
+
+def sinatraa(request):
+    return render(request, 'coaches/sinatraa.html')
+
+def tarik(request):
+    return render(request, 'coaches/tarik.html')
+
+def tenz(request):
+    return render(request, 'coaches/tenz.html')
+
+def xqc(request):
+    return render(request, 'coaches/xqc.html')
+
+def zellsis(request):
+    return render(request, 'coaches/zellsis.html')
