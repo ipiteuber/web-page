3af1d60118b1ec0b0c0793e781add054fc53f0ee
ipiteuber <140553021+ipiteuber@users.noreply.github.com>
2025-04-21 11:32:21 -0400

    Correccion carpeta views

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from core.views.auth import SignUpView, LoginView, LogoutView
+from core.views.auth import SignUpView, LoginView, LogoutView, DeleteAccountView
 
 urlpatterns = [
     path('', views.index, name='index'),
