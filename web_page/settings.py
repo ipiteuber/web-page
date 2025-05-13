e015c80d24569c778a841aa2feab6a59cc0c9c5c
ipiteuber <140553021+ipiteuber@users.noreply.github.com>
2025-05-12 21:06:36 -0400

    Cambio DB a sqlite3

--- web_page/settings.py
+++ web_page/settings.py
@@ -9,23 +9,16 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-from decouple import config
-import oracledb
+
+
 from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Inicializa Oracle Instant Client
-oracledb.init_oracle_client(lib_dir="/home/meow/oracle/instantclient_23_8")
-
-
-# TNS_ADMIN desde .env
-os.environ["TNS_ADMIN"] = config("TNS_ADMIN")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -38,7 +31,7 @@ STEAM_API_KEY = os.getenv("STEAM_API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ipiteuber.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -96,48 +89,27 @@ WSGI_APPLICATION = 'web_page.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
@@ -164,4 +136,3 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'core.User'
-DEBUG = True
--- web_page/wsgi.py
+++ web_page/wsgi.py
@@ -1,9 +1,6 @@
 import os
-import oracledb
+from django.core.wsgi import get_wsgi_application
 
-oracledb.init_oracle_client(lib_dir="/home/meow/oracle/instantclient_23_8")
-os.environ["TNS_ADMIN"] = "/home/meow/oracle_wallet" 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_page.settings")
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
